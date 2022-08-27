d5938b9edd1f40555945b54a5d2d5fd2ae1d9308
Vlad <vlad_aleks93@mail.ru>
2022-08-27 15:13:46 +0300

    added NewFile.js

---
+++ NewFile.js
@@ -0,0 +1,17 @@
+let x = 0;
+let y = 1;
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(x, y));
+
+let temp1 = 100;
+let temp2 = 1;
+
+function diff(c, d) {
+    return c - d;
+}  
+
+console.log(diff(temp1, temp2));
